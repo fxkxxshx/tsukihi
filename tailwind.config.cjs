bc2243ea675de79237e69f3501712d59562e6922
fkxsh <fxkxxshx@gmail.com>
2024-01-02 15:28:52 +0900

    Tailwind CSS カラー・フォントのカスタマイズ

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tsukihi: {
+          black: '#1F1F1F',
+        },
+      },
+      fontFamily: {
+        tsukihiEN: ['owners-xxwide', 'sans-serif'],
+        tsukihiJA: ['fot-udkakugoc80-pro', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
