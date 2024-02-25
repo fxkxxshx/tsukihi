050fa58fc0057414a2ec49b881ae0163477e87ec
fkxsh <fxkxxshx@gmail.com>
2024-02-25 13:37:56 +0900

    一旦完成

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,7 @@ module.exports = {
   settings: {
     tailwindcss: {
       groupByResponsive: true,
+      whitelist: ['^swiper(?:-[a-zA-Z]+)*$'],
     },
   },
 };
---
+++ src/types/place.ts
@@ -0,0 +1,10 @@
+export type PlaceType = {
+  slug: string;
+  date: string;
+  title: string;
+  titleSP: string;
+  titlePC: string;
+  text: string;
+  tags: string[];
+  imgLength: number;
+};
